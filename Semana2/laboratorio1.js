44cb1aa507ed89502051a2a3415877c6290ffc4a
Dev-Stephan <moncadastephan@gmail.com>
2024-09-15 16:39:54 -0600

    Portafolio

---
+++ Semana2/laboratorio1.js
@@ -0,0 +1,43 @@
+var examScore = 95; // Variable de tipo nùmerica
+var postalCode = 11101; // Variable de tipo nùmerica
+var height = 1.75;  // Variable de tipo numerico
+const gender = {
+    male: "H",
+    female: "M"
+};                    // Constante tipo cadena
+const name = "Stephan"; //constante tipo cadena
+var childrenQuantitys = 4; // variable de tipo numerico
+var ISV = 0.15; // Variable de tipo nuemrico
+var weight = 70; // variable de tipo numerico
+const alumnoRepetidor = {
+    repite: "Si",
+    noRepite: "No"
+};                  //constantes de tipo cadena
+var DNI = 0801200033333; //Variable de tipo numerico
+const phoneNumeber = 89568556; //constante de tipo numerica
+var mayorDeEdad = true;
+var mayorDeEdad2 = false;   //Variables de tipo booleano
+const shirtSize = {
+  small: "S",
+  medium: "M",
+  large: "L"
+};                       //Constante de tipo cadena
+
+console.log(examScore);
+console.log(postalCode);
+console.log(height);
+console.log(gender.male);
+console.log(gender.female);
+console.log(name);
+console.log(childrenQuantitys);
+console.log(ISV);
+console.log(weight);
+console.log(alumnoRepetidor.noRepite);
+console.log(alumnoRepetidor.repite);
+console.log(DNI);
+console.log(phoneNumeber);
+console.log(mayorDeEdad);
+console.log(mayorDeEdad2);
+console.log(shirtSize.small);
+console.log(shirtSize.medium);
+console.log(shirtSize.large);
